1ca7e1038d417100aea953970249f6a89eafc39f
Dominik Skerhak <dominik.skerhak@gmail.com>
2020-12-07 09:40:36 +0100

    Refactor Products ConfigsSeeder to use ConfigsTrait
    Part of unification of ConfigsSeeders.
    
    remp/crm#1614

--- src/seeders/ConfigsSeeder.php
+++ src/seeders/ConfigsSeeder.php
@@ -6,133 +6,76 @@ use Crm\ApplicationModule\Builder\ConfigBuilder;
 use Crm\ApplicationModule\Config\ApplicationConfig;
 use Crm\ApplicationModule\Config\Repository\ConfigCategoriesRepository;
 use Crm\ApplicationModule\Config\Repository\ConfigsRepository;
+use Crm\ApplicationModule\Seeders\ConfigsTrait;
 use Crm\ApplicationModule\Seeders\ISeeder;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ConfigsSeeder implements ISeeder
 {
+    use ConfigsTrait;
+
+    private $configBuilder;
+
     private $configCategoriesRepository;
 
     private $configsRepository;
 
-    private $configBuilder;
-
     public function __construct(
+        ConfigBuilder $configBuilder,
         ConfigCategoriesRepository $configCategoriesRepository,
-        ConfigsRepository $configsRepository,
-        ConfigBuilder $configBuilder
+        ConfigsRepository $configsRepository
     ) {
+        $this->configBuilder = $configBuilder;
         $this->configCategoriesRepository = $configCategoriesRepository;
         $this->configsRepository = $configsRepository;
-        $this->configBuilder = $configBuilder;
     }
 
     public function seed(OutputInterface $output)
     {
-        $categoryName = 'products.config.category';
-        $category = $this->configCategoriesRepository->loadByName($categoryName);
-        if (!$category) {
-            $category = $this->configCategoriesRepository->add($categoryName, 'fas fa-shopping-cart', 100);
-            $output->writeln('  <comment>* config category <info>Všeobecne</info> created</comment>');
-        } else {
-            $output->writeln('  * config category <info>Všeobecne</info> exists');
-        }
+        $category = $this->getCategory($output, 'products.config.category', 'fas fa-shopping-cart', 140);
 
-        $name = 'shop_host';
-        $config = $this->configsRepository->loadByName($name);
-        if (!$config) {
-            $this->configBuilder->createNew()
-                ->setName($name)
-                ->setDisplayName('products.config.shop_host.name')
-                ->setDescription('products.config.shop_host.description')
-                ->setType(ApplicationConfig::TYPE_STRING)
-                ->setAutoload(true)
-                ->setConfigCategory($category)
-                ->setSorting(270)
-                ->save();
-            $output->writeln("  <comment>* config item <info>$name</info> created</comment>");
-        } else {
-            $output->writeln("  * config item <info>$name</info> exists");
+        $this->addConfig(
+            $output,
+            $category,
+            'shop_host',
+            ApplicationConfig::TYPE_STRING,
+            'products.config.shop_host.name',
+            'products.config.shop_host.description',
+            null,
+            100
+        );
 
-            if ($config->category->name != $categoryName) {
-                $this->configsRepository->update($config, [
-                    'config_category_id' => $category->id
-                ]);
-                $output->writeln("  * config item <info>$name</info> updated");
-            }
-        }
+        $this->addConfig(
+            $output,
+            $category,
+            'shop_header_block',
+            ApplicationConfig::TYPE_TEXT,
+            'products.config.shop_header_block.name',
+            'products.config.shop_header_block.description',
+            null,
+            200
+        );
 
-        $name = 'shop_header_block';
-        $config = $this->configsRepository->loadByName($name);
-        if (!$config) {
-            $this->configBuilder->createNew()
-                ->setName($name)
-                ->setDisplayName('products.config.shop_header_block.name')
-                ->setDescription('products.config.shop_header_block.description')
-                ->setType(ApplicationConfig::TYPE_TEXT)
-                ->setAutoload(true)
-                ->setConfigCategory($category)
-                ->setSorting(550)
-                ->save();
-            $output->writeln("  <comment>* config item <info>$name</info> created</comment>");
-        } else {
-            $output->writeln("  * config item <info>$name</info> exists");
+        $this->addConfig(
+            $output,
+            $category,
+            'shop_og_image_url',
+            ApplicationConfig::TYPE_STRING,
+            'products.config.shop_og_image_url.name',
+            'products.config.shop_og_image_url.description',
+            null,
+            300
+        );
 
-            if ($config->category->name != $categoryName) {
-                $this->configsRepository->update($config, [
-                    'config_category_id' => $category->id
-                ]);
-                $output->writeln("  * config item <info>$name</info> updated");
-            }
-        }
-
-        $name = 'shop_og_image_url';
-        $config = $this->configsRepository->loadByName($name);
-        if (!$config) {
-            $this->configBuilder->createNew()
-                ->setName($name)
-                ->setDisplayName('products.config.shop_og_image_url.name')
-                ->setDescription('products.config.shop_og_image_url.description')
-                ->setType(ApplicationConfig::TYPE_STRING)
-                ->setAutoload(true)
-                ->setConfigCategory($category)
-                ->setSorting(600)
-                ->save();
-            $output->writeln("  <comment>* config item <info>$name</info> created</comment>");
-        } else {
-            $output->writeln("  * config item <info>$name</info> exists");
-
-            if ($config->category->name != $categoryName) {
-                $this->configsRepository->update($config, [
-                    'config_category_id' => $category->id
-                ]);
-                $output->writeln("  * config item <info>$name</info> updated");
-            }
-        }
-
-        $name = 'shop_terms_and_conditions_url';
-        $config = $this->configsRepository->loadByName($name);
-        if (!$config) {
-            $this->configBuilder->createNew()
-                ->setName($name)
-                ->setDisplayName('products.config.shop_terms_and_conditions_url.name')
-                ->setDescription('products.config.shop_terms_and_conditions_url.description')
-                ->setType(ApplicationConfig::TYPE_STRING)
-                ->setAutoload(true)
-                ->setConfigCategory($category)
-                ->setSorting(700)
-                ->setValue(null)
-                ->save();
-            $output->writeln("  <comment>* config item <info>$name</info> created</comment>");
-        } else {
-            $output->writeln("  * config item <info>$name</info> exists");
-
-            if ($config->category->name != $categoryName) {
-                $this->configsRepository->update($config, [
-                    'config_category_id' => $category->id
-                ]);
-                $output->writeln("  * config item <info>$name</info> updated");
-            }
-        }
+        $this->addConfig(
+            $output,
+            $category,
+            'shop_terms_and_conditions_url',
+            ApplicationConfig::TYPE_STRING,
+            'products.config.shop_terms_and_conditions_url.name',
+            'products.config.shop_terms_and_conditions_url.description',
+            null,
+            400
+        );
     }
 }
