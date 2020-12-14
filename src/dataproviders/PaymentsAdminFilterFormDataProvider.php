87698ab270fef71ff953658f207f26c88c8ca7de
Matus Kalafut <kalafut.matus@gmail.com>
2020-12-14 09:51:30 +0000

    Add payments export to `PaymentsAdminPresenter::default` view
    - **BREAKING**: Change interface `AdminFilterFormDataProviderInterface`. Function `filter()` now accepts array with form values
      - Change `PaymentsAdminFilterFormDataProvider` in `PaymentsModule` and `SalesFunnelModule` to correspond with changes.
    
    - Add CSV payments export to `PaymentsAdminPresenter::default` view. Payments are exported based on filter data
      - Add `ExportsAdminPresenter` to manage created exports.
      - Add `ExportPaymentsHandler` to listen to `export-payments` Hermes event and export payments to file.
    
    - Add Flysystem package and `ApplicationMountManager` for filesystem initialization for use in whole application
    
    remp/crm#1600

--- src/dataproviders/PaymentsAdminFilterFormDataProvider.php
+++ src/dataproviders/PaymentsAdminFilterFormDataProvider.php
@@ -6,7 +6,6 @@ use Crm\ApplicationModule\DataProvider\DataProviderException;
 use Crm\PaymentsModule\DataProvider\AdminFilterFormDataProviderInterface;
 use Crm\ProductsModule\PaymentItem\ProductPaymentItem;
 use Crm\ProductsModule\Repository\ProductsRepository;
-use Nette\Application\Request;
 use Nette\Application\UI\Form;
 use Nette\Database\Table\Selection;
 
@@ -32,34 +31,39 @@ class PaymentsAdminFilterFormDataProvider implements AdminFilterFormDataProvider
             throw new DataProviderException('invalid type of provided form: ' . get_class($params['form']));
         }
 
-        if (!isset($params['request'])) {
-            throw new DataProviderException('missing [request] within data provider params');
+        if (!isset($params['formData'])) {
+            throw new DataProviderException('missing [formData] within data provider params');
         }
-        if (!($params['request'] instanceof Request)) {
-            throw new DataProviderException('invalid type of provided request: ' . get_class($params['request']));
+        if (!is_array($params['formData'])) {
+            throw new DataProviderException('invalid type of provided formData: ' . get_class($params['formData']));
         }
 
         $form = $params['form'];
-        $request = $params['request'];
+        $formData = $params['formData'];
 
         $products = $this->productsRepository->getTable()->fetchPairs('id', 'name');
         $form->addMultiSelect('products', 'Produkty', $products)
             ->getControlPrototype()->addAttributes(['class' => 'select2']);
 
         $form->setDefaults([
-            'products' => $request->getParameter('products'),
+            'products' => $this->getProducts($formData)
         ]);
 
         return $form;
     }
 
-    public function filter(Selection $selection, Request $request): Selection
+    public function filter(Selection $selection, array $formData): Selection
     {
-        if ($request->getParameter('products')) {
+        if ($this->getProducts($formData)) {
             $selection
                 ->where(':payment_items.type', ProductPaymentItem::TYPE)
-                ->where(':payment_items.product_id', $request->getParameter('products'));
+                ->where(':payment_items.product_id', $this->getProducts($formData));
         }
         return $selection;
+    }
+
+    private function getProducts($formData)
+    {
+        return $formData['products'] ?? null;
     }
 }
