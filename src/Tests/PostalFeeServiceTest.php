7c1c9112aef98de484fffbb4b8120d41b5fdf29b
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-11-15 10:01:42 +0100

    Fix hardcoded country codes in tests
    ID of countries can be different on different instancies (eg. if running
    tests above truncated table, seed will create countries with higher IDs).
    
    Using ISO codes instead IDs is better anyway - we are not testing if
    countries were seeded with "correct" IDS.
    
    remp/crm#2111

--- src/Tests/PostalFeeServiceTest.php
+++ src/Tests/PostalFeeServiceTest.php
@@ -9,6 +9,7 @@ use Crm\ProductsModule\PostalFeeCondition\PostalFeeService;
 use Crm\ProductsModule\Repository\CountryPostalFeeConditionsRepository;
 use Crm\ProductsModule\Repository\CountryPostalFeesRepository;
 use Crm\ProductsModule\Repository\PostalFeesRepository;
+use Crm\UsersModule\Repository\CountriesRepository;
 use Mockery\Mock;
 use Nette\Database\Table\IRow;
 
@@ -23,12 +24,16 @@ class PostalFeeServiceTest extends DatabaseTestCase
     /** @var CountryPostalFeeConditionsRepository */
     private $countryPostalFeeConditionsRepository;
 
+    /** @var CountriesRepository */
+    private $countriesRepository;
+
     protected function requiredRepositories(): array
     {
         return [
             CountryPostalFeesRepository::class,
             PostalFeesRepository::class,
             CountryPostalFeeConditionsRepository::class,
+            CountriesRepository::class,
         ];
     }
 
@@ -46,33 +51,34 @@ class PostalFeeServiceTest extends DatabaseTestCase
         $this->postalFeesRepository = $this->getRepository(PostalFeesRepository::class);
         $this->countryPostalFeesRepository = $this->getRepository(CountryPostalFeesRepository::class);
         $this->countryPostalFeeConditionsRepository = $this->getRepository(CountryPostalFeeConditionsRepository::class);
+        $this->countriesRepository = $this->getRepository(CountriesRepository::class);
     }
 
     public function testGetAvailablePostalOptionsWithoutPostalFeeConditions(): void
     {
-        $this->preparePostalFeeForCountry(164, 'posta_list', 1.99);
-        $this->preparePostalFeeForCountry(164, 'dhl_parcel', 1.99);
-        $this->preparePostalFeeForCountry(164, 'ups_parcel', 1.99);
+        $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'posta_list', 1.99);
+        $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'dhl_parcel', 1.99);
+        $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'ups_parcel', 1.99);
 
-        $this->preparePostalFeeForCountry(32, 'dhl_parcel', 1.99);
-        $this->preparePostalFeeForCountry(32, 'ups_parcel', 1.99);
+        $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('CZ')->id, 'dhl_parcel', 1.99);
+        $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('CZ')->id, 'ups_parcel', 1.99);
 
         /** @var PostalFeeService $postalFeeService */
         $postalFeeService = $this->inject(PostalFeeService::class);
 
-        $postalFees = $postalFeeService->getAvailablePostalFeesOptions(164, []);
+        $postalFees = $postalFeeService->getAvailablePostalFeesOptions($this->countriesRepository->findByIsoCode('SK')->id, []);
 
         $this->assertCount(3, $postalFees);
     }
 
     public function testGetAvailablePostalOptionsWithPostalFeeConditions(): void
     {
-        $this->preparePostalFeeForCountry(164, 'posta_list', 1.99);
+        $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'posta_list', 1.99);
 
-        $postalFeeRow = $this->preparePostalFeeForCountry(164, 'dhl_parcel', 1.99);
+        $postalFeeRow = $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'dhl_parcel', 1.99);
         $this->countryPostalFeeConditionsRepository->add($postalFeeRow, 'test_code', 120);
 
-        $this->preparePostalFeeForCountry(164, 'ups_parcel', 1.99);
+        $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'ups_parcel', 1.99);
 
 
         /** @var PostalFeeConditionInterface|Mock $postalFeeConditionMock */
@@ -85,7 +91,7 @@ class PostalFeeServiceTest extends DatabaseTestCase
         $postalFeeService = $this->inject(PostalFeeService::class);
         $postalFeeService->registerCondition('test_code', $postalFeeConditionMock);
 
-        $postalFees = $postalFeeService->getAvailablePostalFeesOptions(164, []);
+        $postalFees = $postalFeeService->getAvailablePostalFeesOptions($this->countriesRepository->findByIsoCode('SK')->id, []);
 
         $this->assertCount(2, $postalFees);
         $this->assertEquals(['posta_list', 'ups_parcel'], array_column($postalFees, 'code'));
@@ -99,17 +105,17 @@ class PostalFeeServiceTest extends DatabaseTestCase
         $postalFeeService = new PostalFeeService($this->countryPostalFeesRepository);
         $postalFeeService->registerCondition('test_code', $postalFeeConditionMock);
 
-        $postalFees = $postalFeeService->getAvailablePostalFeesOptions(164, []);
+        $postalFees = $postalFeeService->getAvailablePostalFeesOptions($this->countriesRepository->findByIsoCode('SK')->id, []);
         $this->assertCount(3, $postalFees);
     }
 
     public function testGetAvailablePostalOptionsWithMorePostalFeesOfSameType(): void
     {
-        $this->preparePostalFeeForCountry(164, 'posta_list', 1.99);
-        $this->preparePostalFeeForCountry(164, 'dhl_parcel', 1.99);
-        $this->preparePostalFeeForCountry(164, 'ups_parcel', 1.99);
+        $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'posta_list', 1.99);
+        $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'dhl_parcel', 1.99);
+        $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'ups_parcel', 1.99);
 
-        $postalFeeRow = $this->preparePostalFeeForCountry(164, 'dhl_parcel', 0, 20);
+        $postalFeeRow = $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'dhl_parcel', 0, 20);
         $this->countryPostalFeeConditionsRepository->add($postalFeeRow, 'test_code', 120);
 
         /** @var PostalFeeConditionInterface|Mock $postalFeeConditionMock */
@@ -122,7 +128,7 @@ class PostalFeeServiceTest extends DatabaseTestCase
         $postalFeeService = $this->inject(PostalFeeService::class);
         $postalFeeService->registerCondition('test_code', $postalFeeConditionMock);
 
-        $postalFees = $postalFeeService->getAvailablePostalFeesOptions(164, []);
+        $postalFees = $postalFeeService->getAvailablePostalFeesOptions($this->countriesRepository->findByIsoCode('SK')->id, []);
         $this->assertCount(3, $postalFees);
         $this->assertEquals([1.99, 1.99, 0], array_column($postalFees, 'amount'));
     }
@@ -133,11 +139,11 @@ class PostalFeeServiceTest extends DatabaseTestCase
         $postalFeeService = $this->inject(PostalFeeService::class);
 
         // From paid choose default
-        $postalFee1Row = $this->preparePostalFeeForCountry(164, 'posta_list', 2.99, 10, false);
-        $postalFee2Row = $this->preparePostalFeeForCountry(164, 'dhl_parcel', 2.99, 10, true);
-        $postalFee3Row = $this->preparePostalFeeForCountry(164, 'ups_parcel', 2.99, 10, false);
+        $postalFee1Row = $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'posta_list', 2.99, 10, false);
+        $postalFee2Row = $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'dhl_parcel', 2.99, 10, true);
+        $postalFee3Row = $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('SK')->id, 'ups_parcel', 2.99, 10, false);
 
-        $defaultPostalFeeRow = $postalFeeService->getDefaultPostalFee(164, [
+        $defaultPostalFeeRow = $postalFeeService->getDefaultPostalFee($this->countriesRepository->findByIsoCode('SK')->id, [
             $postalFee1Row->id => $postalFee1Row,
             $postalFee2Row->id => $postalFee2Row,
             $postalFee3Row->id => $postalFee3Row,
@@ -145,11 +151,11 @@ class PostalFeeServiceTest extends DatabaseTestCase
         $this->assertEquals($postalFee2Row->id, $defaultPostalFeeRow->id);
 
         // From paid and free choose free
-        $postalFee4Row = $this->preparePostalFeeForCountry(165, 'posta_list', 2.99, 10, true);
-        $postalFee5Row = $this->preparePostalFeeForCountry(165, 'dhl_parcel', 2.99, 10, false);
-        $postalFee6Row = $this->preparePostalFeeForCountry(165, 'ups_parcel', 0, 10, false);
+        $postalFee4Row = $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('PL')->id, 'posta_list', 2.99, 10, true);
+        $postalFee5Row = $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('PL')->id, 'dhl_parcel', 2.99, 10, false);
+        $postalFee6Row = $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('PL')->id, 'ups_parcel', 0, 10, false);
 
-        $defaultPostalFeeRow = $postalFeeService->getDefaultPostalFee(165, [
+        $defaultPostalFeeRow = $postalFeeService->getDefaultPostalFee($this->countriesRepository->findByIsoCode('PL')->id, [
             $postalFee4Row->id => $postalFee4Row,
             $postalFee5Row->id => $postalFee5Row,
             $postalFee6Row->id => $postalFee6Row,
@@ -157,18 +163,18 @@ class PostalFeeServiceTest extends DatabaseTestCase
         $this->assertEquals($postalFee6Row->id, $defaultPostalFeeRow->id);
 
         // From all free choose default free
-        $postalFee7Row = $this->preparePostalFeeForCountry(166, 'posta_list', 0, 10, false);
-        $postalFee8Row = $this->preparePostalFeeForCountry(166, 'dhl_parcel', 0, 10, true);
-        $postalFee9Row = $this->preparePostalFeeForCountry(166, 'ups_parcel', 0, 10, false);
+        $postalFee7Row = $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('UA')->id, 'posta_list', 0, 10, false);
+        $postalFee8Row = $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('UA')->id, 'dhl_parcel', 0, 10, true);
+        $postalFee9Row = $this->preparePostalFeeForCountry($this->countriesRepository->findByIsoCode('UA')->id, 'ups_parcel', 0, 10, false);
 
-        $defaultPostalFeeRow = $postalFeeService->getDefaultPostalFee(166, [
+        $defaultPostalFeeRow = $postalFeeService->getDefaultPostalFee($this->countriesRepository->findByIsoCode('UA')->id, [
             $postalFee7Row->id => $postalFee7Row,
             $postalFee8Row->id => $postalFee8Row,
             $postalFee9Row->id => $postalFee9Row,
         ]);
         $this->assertEquals($postalFee8Row->id, $defaultPostalFeeRow->id);
     }
-    
+
     private function preparePostalFeeForCountry(int $countryId, string $code, float $amount, int $sorting = 10, bool $default = false): IRow
     {
         $postalFeeRow = $this->postalFeesRepository->add($code, $code, $amount);
