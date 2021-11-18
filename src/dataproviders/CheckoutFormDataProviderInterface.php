dcee87ba86e512bf5d1edfa28cc8cb62ccc9ee2b
Peter Dulacka <dulacka@gmail.com>
2021-11-18 14:19:57 +0100

    Release 0.36.0

--- src/dataproviders/CheckoutFormDataProviderInterface.php
+++ src/dataproviders/CheckoutFormDataProviderInterface.php
@@ -11,5 +11,12 @@ interface CheckoutFormDataProviderInterface extends DataProviderInterface
 
     public function formSucceeded($form, $values, array $params);
 
+    /**
+     * addAdditionalColumns serves for storing extra columns in the `orders` table. In case your dataprovider needs
+     * to store extra data in the orders table, alter $additionalColumns variable and add your value as you'd set
+     * it in the Repository::update() method.
+     *
+     *      $additionalColumns['foo'] = 'bar' // attempts to store 'bar' to the orders.foo column
+     */
     public function addAdditionalColumns($form, $values, &$additionalColumns);
 }
