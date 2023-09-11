b9f6fe94855a6e887a37e062bf2a2fddefeae631
Dominik Skerhak <dominik.skerhak@gmail.com>
2023-09-11 12:31:51 +0200

    Fix down migration for ChangeProductTagsSortingNonNullable
    remp/crm@26225e0ad489eea8fcebd49580966cb15fc1af74
    
    remp/helpdesk#2068

--- src/migrations/20230906113207_change_product_tags_sorting_non_nullable.php
+++ src/migrations/20230906113207_change_product_tags_sorting_non_nullable.php
@@ -5,12 +5,19 @@ use Phinx\Migration\AbstractMigration;
 
 final class ChangeProductTagsSortingNonNullable extends AbstractMigration
 {
-    public function change(): void
+    public function up(): void
     {
         $this->query("UPDATE product_tags SET sorting = 100 WHERE sorting IS NULL;");
 
         $this->table('product_tags')
             ->changeColumn('sorting', 'integer', ['null' => false, 'default' => 100])
+            ->update();
+    }
+
+    public function down(): void
+    {
+        $this->table('product_tags')
+            ->changeColumn('sorting', 'integer', ['null' => true, 'default' => 100]) // revert not NULLABLE
             ->update();
     }
 }
